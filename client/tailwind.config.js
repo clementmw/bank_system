825b303f29ec11ddd30e1a37cf165dd4e0bd9b32
Clement Macharia <clementmacharia62@gmail.com>
2024-02-23 02:15:30 +0300

    added tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  // darkMode: 'selector',
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors:{
+        customColor:'#655546',
+        coffeeColor:'#634832',
+
+      },
+    },
+  },
+  screens: {
+    'sm': '640px',
+    // => @media (min-width: 640px) { ... }
+
+    'md': '768px',
+    // => @media (min-width: 768px) { ... }
+
+    'lg': '1024px',
+    // => @media (min-width: 1024px) { ... }
+
+    'xl': '1280px',
+    // => @media (min-width: 1280px) { ... }
+
+    '2xl': '1536px',
+    // => @media (min-width: 1536px) { ... }
+  },
+  plugins: [],
+}
