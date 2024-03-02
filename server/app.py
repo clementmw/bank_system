9b220d116ba02757826b2dd899ddb22cbfcc564c
Clement Macharia <clementmacharia62@gmail.com>
2024-03-03 00:32:02 +0300

    deployment

--- server/app.py
+++ server/app.py
@@ -27,8 +27,8 @@ app = Flask(
 
 app.config['JWT_SECRET_KEY'] = b'\xb2\xd3B\xb9 \xab\xc0By\x13\x10\x84\xb7M!\x11'
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 24 * 60 * 60
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bank.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI') #render database url
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bank.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI') #render database url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 # app.secret_key = b'\xb2\xd3B\xb9 \xab\xc0By\x13\x10\x84\xb7M!\x11'
