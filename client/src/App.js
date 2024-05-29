62cb6fb6c3bd3106418b9fbfef29c88e3e46f940
Clement Macharia <clementmacharia62@gmail.com>
2024-05-30 01:40:09 +0300

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import User from './components/User';
 import Transaction from './components/Transaction';
 import TransactionForm from './components/TransactionForm';
 import Account from './components/Account';
+import NotFound from './Reusable/NotFound';
 
 
 
@@ -22,7 +23,7 @@ function App() {
        
       <Routes>
       <Route path="/" element={<Home />} /> 
-        <Route path = '/signup' element={<Register/>}/>
+        <Route path = '/register' element={<Register/>}/>
         <Route path= '/login' element={<Login />}/>
         <Route path = '/testimonial' element={<Testimonial/>}/>
         <Route path = '/contact' element={<Contact/>}/>
@@ -31,6 +32,7 @@ function App() {
         <Route path='/transaction' element = {<Transaction/>}/>
         <Route path = '/transfer' element = {<TransactionForm/>}/>
         <Route path = '/newaccount' element = {<Account/>}/>
+        <Route path = '*' element = {<NotFound/>}/>
 
       </Routes>
     </div>
